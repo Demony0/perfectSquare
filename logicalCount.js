c130b49873390d17e4d411311608c8da2deff430
Demony0 <75173864+Demony0@users.noreply.github.com>
2021-02-09 08:18:57 -0800

    Add files via upload

--- logicalCount.js
+++ logicalCount.js
@@ -1,40 +1,37 @@
 var cell=[];
-// var userInput=3;
-// let x=Math.round(userInput/2)-1;
-// let y = 0;
 var empty=0;
-// let holdX=x;
-// let holdY=y;
+var tempt=0;
 let holduserInput = "";
 var table = document.getElementById('tables');
+var resetBtn = document.getElementById('reset');
+var submitBtn = document.getElementById('submit');
 
 
 function getNumber(){
 	let userInput=document.getElementById("number").value;
 	let x=Math.round(userInput/2)-1;
 	let y = 0;
-	if (holduserInput!=userInput){
-		if (userInput%2===1){
-			let i=userInput;
-			let j=userInput;
-			tableWithEmptyCell(x,y,userInput);
-			for (let i = 0 ; i < userInput ; i++){
-				var row = tables.insertRow(i);
-				for (let j = 0 ; j < userInput ; j++){
-					let box=row.insertCell(j);
-					box.innerHTML=cell[i][j];
+	if (userInput%2===1){
+		let i=userInput;
+		let j=userInput;
+		
+			if(resetBtn.disable==true){
+		
+				tableWithEmptyCell(x,y,userInput);
+				for (let i = 0 ; i < userInput ; i++){
+					var row = tables.insertRow(i);
+					for (let j = 0 ; j < userInput ; j++){
+						let box=row.insertCell(j);
+						box.innerHTML=cell[i][j];
+					}
 				}
 			}
+			
+			
 		}else{
-			alert("You Input an Even Number");
-		}
-	}
-	else{
-		
-		alert("Input New Even Number or Empty Input")
-	}holduserInput = userInput;
+			alert("You Input an Even Number Or Missing Input");
+	}	
 }
-
 
 function tableWithEmptyCell(x,y,userInput){
 	// table
@@ -72,14 +69,19 @@ function tableWithEmptyCell(x,y,userInput){
 			y=userInput-1;
 		}
 		count++;
-	 }while(count<userInput*userInput+1);;
+	 }while(count<userInput*userInput+1);
+	 resetBtn.disable=false;
+	 submitBtn.disable=false;
 }
 
 
 
 function clearNumber(){
 	userInput=document.getElementById("number").value;
-	for (let i = 0 ; i < userInput ; i++){
-			table.deleteRow(0);
+	if(resetBtn.disable==false){
+		for (let i = 0 ; i < userInput ; i++){
+				table.deleteRow(0);
+		}submitBtn.disable=false;
 	}
+	resetBtn.disable=true;
 }
